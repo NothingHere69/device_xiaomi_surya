dcb14b9d05fbabea4c0a94a645f42e98aea6be31
Bin <9633157+web1n@users.noreply.github.com>
2025-06-16 23:40:24 +0800

    sweet: Patch audio to load libaudioroute-v34.so
    Change-Id: Idad20512982b8753820fffa6d956df137e462638

--- extract-files.py
+++ extract-files.py
@@ -44,6 +44,12 @@ lib_fixups: lib_fixups_user_type = {
 blob_fixups: blob_fixups_user_type = {
     'vendor/lib64/android.hardware.camera.provider@2.4-legacy.so': blob_fixup()
         .add_needed('libcamera_provider_shim.so'),
+    'vendor/etc/init/init.batterysecret.rc': blob_fixup()
+        .regex_replace(' +seclabel u:r:batterysecret:s0\n', ''),
+    'vendor/etc/init/init.mi_thermald.rc': blob_fixup()
+        .regex_replace(' +seclabel u:r:mi_thermald:s0\n', ''),
+    ('vendor/lib/hw/audio.primary.sm6150.so', 'vendor/lib/libaudioroute_ext.so'): blob_fixup()
+        .replace_needed('libaudioroute.so', 'libaudioroute-v34.so'),
     'vendor/lib64/camera/components/com.qti.node.watermark.so': blob_fixup()
         .add_needed('libpiex_shim.so'),
     'vendor/lib64/libalRnBRT_GL_GBWRAPPER.so': blob_fixup()
